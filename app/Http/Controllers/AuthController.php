5595a4acb3f8f25d5c710af3791c6fe839f7bc35
Berkay Tikenoğlu <berkaytikenoglu@gmail.com>
2024-08-20 13:50:44 +0300

    İlk Commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+  
+     public function store(Request $request)
+     {
+         try {
+             $validatedData = $request->validate([
+                 'tc_identity' => 'required|string|max:255',
+                 'firstname' => 'required|string|max:255',
+                 'lastname' => 'required|string|max:255',
+                 'phonenumber' => 'required|string|max:255',
+                 'email' => 'required|string|email|max:255|unique:users',
+                 'password' => 'required|string|min:8',
+             ]);
+
+           
+             $user = User::create([
+                 'tc_identity' => $validatedData['tc_identity'],
+                 'firstname' => $validatedData['firstname'],
+                 'lastname' => $validatedData['lastname'],
+                 'phonenumber' => $validatedData['phonenumber'],
+                 'name' => $validatedData['firstname'],
+                 'email' => $validatedData['email'],
+                 'password' => Hash::make($validatedData['password']),
+             ]);
+     
+             return response()->json([
+                 'message' => 'User successfully created!',
+                 'user' => $user
+             ], 201);
+         } catch (\Illuminate\Validation\ValidationException $e) {
+             return response()->json([
+                 'errors' => $e->errors()
+             ], 422);
+         }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+
+        $user = User::findOrFail($id);
+        return response()->json($user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id); // ID'ye göre kullanıcıyı bul
+
+        // Validate incoming data
+        $validated = $request->validate([
+            'name' => 'string|max:255',
+            'email' => 'string|email|max:255|unique:users,email,' . $user->id,
+            'password' => 'nullable|string|min:8',
+        ]);
+    
+        // Update fields
+        $user->name = $validated['name'] ?? $user->name;
+        $user->email = $validated['email'] ?? $user->email;
+        if (!empty($request->input('password'))) {
+            $user->password = Hash::make($request->input('password')); // Şifreyi güncelle
+        }
+        $user->save(); // Değişiklikleri veritabanına kaydet
+    
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
